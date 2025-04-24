38d6921301baa85f6dd74ea8880072107c21f86f
haseeb <haseeb.arshad@spadasoftinc.com>
2025-04-24 19:51:27 +0500

    7

---
+++ src/components/layout/footer/AppDownloadLink.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+// components
+
+// types
+type AppDownloadLinkPropType = {
+  imageSrc: string;
+  altText: string;
+  linkText: string;
+  linkTextBold: string;
+  link: string;
+};
+
+const AppDownloadLink = ({
+  imageSrc,
+  altText,
+  linkText,
+  linkTextBold,
+  link,
+}: AppDownloadLinkPropType) => {
+  return (
+    <>
+      <a
+        href={link}
+        className="w-[max-content] h-[max-content] mb-5 flex items-center py-7 px-8 rounded-medium bg-black-smooth min-w-[230px] max-w-[230px]"
+      >
+        <Image
+          src={`/images/${imageSrc}`}
+          alt={altText}
+          width={35}
+          height={35}
+          className="pr-4 border-r-1 border-white-secondary/70"
+        />
+        <p className="text-white font-normal text-[13px] leading-4 capitalize pl-4">
+          {linkText} <br />
+          <span className="font-medium text-[16px] leading-[18px] mt-[7px] inline-reset">
+            {linkTextBold}
+          </span>
+        </p>
+      </a>
+    </>
+  );
+};
+export default AppDownloadLink;
--- src/components/layout/footer/FooterLinks.tsx
+++ src/components/layout/footer/FooterLinks.tsx
@@ -1,24 +1,47 @@
 // components
 import { FooterLinksContent } from "@/components/homepage/content/content";
+import Image from "next/image";
+import AppDownloadLink from "./AppDownloadLink";
 
 const FooterLinks = () => {
   return (
     <>
-      {FooterLinksContent.map((item, idx) => (
-        <div className="pr-[50px] text-white" key={idx}>
-          <h6 className="footer-links-heading">{item.tab}</h6>
+      <div className="flex justify-between flex-1">
+        {FooterLinksContent.map((item, idx) => (
+          <div className="pr-[50px] text-white" key={idx}>
+            <h6 className="footer-links-heading">{item.tab}</h6>
 
-          {item.links.map((link, idxx) => (
-            <a
-              href="#"
-              key={idxx}
-              className="block w-[max-content] font-normal text-[15px] leading-7 mb-[15px] capitalize max-w-[170px]"
-            >
-              {link}
-            </a>
-          ))}
+            {item.links.map((link, idxx) => (
+              <a
+                href="#"
+                key={idxx}
+                className="block w-[max-content] font-normal text-[15px] leading-7 mb-[15px] capitalize max-w-[170px]"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+        ))}
+
+        <div className="text-white">
+          <h6 className="footer-links-heading">get the app</h6>
+
+          <AppDownloadLink
+            imageSrc={"apple.png"}
+            altText={"app-store"}
+            linkText={"download on the"}
+            linkTextBold={"apple store"}
+            link={"/"}
+          />
+          <AppDownloadLink
+            imageSrc={"play.png"}
+            altText={"play-store"}
+            linkText={"get it on"}
+            linkTextBold={"gogole play"}
+            link={"/"}
+          />
         </div>
-      ))}
+      </div>
     </>
   );
 };
